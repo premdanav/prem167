6d0aa2286d94c513bc5011554be84d09bcb1fa54
premdanav <premdanav@gmail.com>
2023-07-25 09:06:10 +0530

    added codes

---
+++ LargestNum.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class LargestNum {
+
+    public static void main(String[] args) {
+
+         Scanner sc=new Scanner (System.in);
+         System.out.println("Enter the number1: ");
+         int n1=sc.nextInt();
+         System.out.println("Enter the number2: ");
+         int n2=sc.nextInt();
+         System.out.println("Enter the number3: ");
+         int n3=sc.nextInt();
+
+        if( (n1+n2) >= (n2+n3))
+            System.out.println(n1+" and "+n2  + " has the largest sum="+(n1+n2));
+
+        else if ((n1+n3) >= (n2+n3))
+            System.out.println(n1+" and "+n3  + " has the largest sum="+(n1+n3));
+
+        else
+            System.out.println(n2+" and "+n3  + " has the largest sum="+(n2+n3));
+
+        
+        
+    }
+}
+              
